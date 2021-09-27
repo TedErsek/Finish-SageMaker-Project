f7a0c1f219adc9157b08d54cfb8251e161491702
TedErsek <75995490+TedErsek@users.noreply.github.com>
2021-09-27 12:54:25 -0400

    Add files via upload

--- predict.py
+++ predict.py
@@ -89,9 +89,12 @@ def predict_fn(self, input_data, model):
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
     with torch.no_grad():
-        output = model(data)
-        result = int(np.round(output.data.cpu().numpy))    
+        oputput = model.forward(data)
+    result = np.round(output.numpy()).astype(int)
     return result
+    #     output = model(data)
+    #     result = int(np.round(output.data.cpu().numpy))    
+    #return result
 
 
 
